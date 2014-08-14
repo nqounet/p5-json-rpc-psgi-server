95492e3fb67b3ea819b05173694fc7671b48d875
nqounet <mail@nqou.net>
2014-08-15 01:46:55 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+JSON::RPC::PSGI::Server - It's new $module
+
+# SYNOPSIS
+
+    use JSON::RPC::PSGI::Server;
+
+# DESCRIPTION
+
+JSON::RPC::PSGI::Server is ...
+
+# LICENSE
+
+Copyright (C) nqounet.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+nqounet <mail@nqou.net>
---
+++ lib/JSON/RPC/PSGI/Server.pm
@@ -0,0 +1,39 @@
+package JSON::RPC::PSGI::Server;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+JSON::RPC::PSGI::Server - It's new $module
+
+=head1 SYNOPSIS
+
+    use JSON::RPC::PSGI::Server;
+
+=head1 DESCRIPTION
+
+JSON::RPC::PSGI::Server is ...
+
+=head1 LICENSE
+
+Copyright (C) nqounet.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+nqounet E<lt>mail@nqou.netE<gt>
+
+=cut
+
